e65ca9ec6adc9dced266efe5004c9f070fd383ae
Martina Beleva <martina_bel@abv.bg>
2024-08-18 14:45:54 +0300

    register user template created

--- link-up-app/src/views/Register/Register.jsx
+++ link-up-app/src/views/Register/Register.jsx
@@ -1,14 +1,13 @@
-import { useContext, useState } from "react"
-import { registerUser } from "../../services/auth.service"
-import { AppContext } from "../../state/app.context"
-import { useNavigate } from "react-router-dom"
-import { createUserUsername, getUserByUsername } from "../../services/users.service"
-
+import { useContext, useState } from "react";
+import { registerUser } from "../../services/auth.service";
+import { AppContext } from "../../state/app.context";
+import { useNavigate } from "react-router-dom";
+import { createUserUsername, getUserByUsername } from "../../services/users.service";
 
 export default function Register() {
     const [user, setUser] = useState({
         username: '',
-        phone: '', 
+        phone: '',
         email: '',
         password: '',
         confirmPassword: '',
@@ -23,7 +22,8 @@ export default function Register() {
         });
     };
 
-    const register = async () => {
+    const register = async (e) => {
+        e.preventDefault();
 
         if (!user.email.trim() || !user.password) {
             return console.error('No credentials provided!');
@@ -36,54 +36,142 @@ export default function Register() {
         if (user.username.length < 5 || user.username.length > 35) {
             return console.error('Invalid username length');
         }
-    
-        try {
-            // const userDB = await getUserByEmail(user.email.trim());
-            // if (userDB) {
-            //     return console.error(`User {${user.email}} already exists!`);
-            // }
 
+        try {
             const userFromDB = await getUserByUsername(user.username);
             if (userFromDB) {
                 return console.error(`User {${user.username}} already exists!`);
             }
+
             const credential = await registerUser(user.email.trim(), user.password.trim());
             await createUserUsername(user.username, credential.user.uid, user.email, user.phone);
             setAppState({ user: credential.user, userData: null });
-            navigate('/');
-            console.success('Successfully registered');
+            navigate('/'); 
+            console.log('Successfully registered');
         } catch (error) {
             console.error(error.message);
         }
     };
 
-
     return (
-        <>
-            <div className="registerContainer">
-                <h1 className="registerTitle">Register</h1>
-
-                <label className="registerLabel" htmlFor="handle">Username: </label>
-                <input className="registerInput" placeholder='Create a username...' value={user.handle} onChange={updateUser('handle')} type="text" name="handle" id="handle" /><br />
-
-                <label className="registerLabel" htmlFor="firstName">First name: </label>
-                <input className="registerInput" placeholder='Create a first name...' value={user.firstName} onChange={updateUser('firstName')} type="text" name="firstName" id="firstName" /><br />
-
-                <label className="registerLabel" htmlFor="lastName">Last name: </label>
-                <input className="registerInput" placeholder='Create a last name...' value={user.lastName} onChange={updateUser('lastName')} type="text" name="lastName" id="lastName" /><br />
-
-                <label className="registerLabel" htmlFor="email">Email: </label>
-                <input className="registerInput" placeholder='Create email...' value={user.email} onChange={updateUser('email')} type="text" name="email" id="email" /><br />
-
-                <label className="registerLabel" htmlFor="password">Password: </label>
-                <input className="registerInput" placeholder='Create a password...' value={user.password} onChange={updateUser('password')} type="password" name="password" id="password" /><br />
-
-                <label className="registerLabel" htmlFor="confirmPassword">Confirm Password: </label>
-                <input className="registerInput" placeholder='Confirm the password...' value={user.confirmPassword} onChange={updateUser('confirmPassword')} type="password" name="confirmPassword" id="confirmPassword" /><br />
-
-                <button className="registerButton" onClick={register}>Register</button>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                    alt="Your Company"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    className="mx-auto h-10 w-auto"
+                />
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    Sign up
+                </h2>
             </div>
-        </>
 
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form onSubmit={register} className="space-y-6">
+
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            Email address
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                required
+                                placeholder='Type your email...'
+                                value={user.email}
+                                autoComplete="email"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                onChange={updateUser('email')}
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                            Username
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="username"
+                                name="username"
+                                type="text"
+                                required
+                                autoComplete="username"
+                                placeholder='Your username...'
+                                value={user.username}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                onChange={updateUser('username')}
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="phone" className="block text-sm font-medium leading-6 text-gray-900">
+                            Phone
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="phone"
+                                name="phone"
+                                type="tel"
+                                required
+                                placeholder='Phone number...'
+                                value={user.phone}
+                                autoComplete="phone"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                onChange={updateUser('phone')}
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                            Password
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                required
+                                autoComplete="current-password"
+                                value={user.password}
+                                onChange={updateUser('password')}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="confirmPassword" className="block text-sm font-medium leading-6 text-gray-900">
+                            Confirm password
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="confirmPassword"
+                                name="confirmPassword"
+                                type="password"
+                                required
+                                autoComplete="current-password"
+                                value={user.confirmPassword}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                onChange={updateUser('confirmPassword')}
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Sign up
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
