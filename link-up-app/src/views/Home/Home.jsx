a80d27b91dd0e6f9a21ea4622f6a761d568cd34a
Ivo <hristovivo24@gmail.com>
2024-08-20 15:06:43 +0300

    home body implemented

--- link-up-app/src/views/Home/Home.jsx
+++ link-up-app/src/views/Home/Home.jsx
@@ -92,9 +92,44 @@ export default function Home() {
                 </nav>
             </div>
 
-            <div className="flex-1 p-8">
-                {/* Content goes here */}
+            {/* Main Content */}
+            <div className="flex flex-1 p-8 bg-gray-900 text-white space-x-6">
+                {/* Channels Section */}
+                <div className="w-1/4 space-y-6">
+                    {/* Text Channels */}
+                    <div className="bg-gray-800 p-4 rounded-lg">
+                        <h3 className="text-lg font-semibold mb-2">Text Channels</h3>
+                        <div className="space-y-2">
+                            <button className="w-full p-2 text-left bg-gray-700 rounded-md hover:bg-gray-600"># general</button>
+                            <button className="w-full p-2 text-left bg-gray-700 rounded-md hover:bg-gray-600"># memes</button>
+                            <button className="w-full p-2 text-left bg-gray-700 rounded-md hover:bg-gray-600"># announcements</button>
+                        </div>
+                    </div>
+
+                    {/* Voice Channels */}
+                    <div className="bg-gray-800 p-4 rounded-lg">
+                        <h3 className="text-lg font-semibold mb-2">Voice Channels</h3>
+                        <div className="space-y-2">
+                            <button className="w-full p-2 text-left bg-gray-700 rounded-md hover:bg-gray-600">🔊 General</button>
+                            <button className="w-full p-2 text-left bg-gray-700 rounded-md hover:bg-gray-600">🔊 Gaming</button>
+                            <button className="w-full p-2 text-left bg-gray-700 rounded-md hover:bg-gray-600">🔊 Music</button>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Chat Section */}
+                <div className="flex-1 bg-gray-800 p-6 rounded-lg flex flex-col">
+                    {/* Chat Team Name */}
+                    <h1 className="text-2xl font-bold mb-4">Chat Team Name</h1>
+
+                    {/* Messages Container */}
+                    <div className="flex-1 bg-gray-700 p-4 rounded-lg overflow-y-auto flex flex-col-reverse">
+                        {/* Chat messages will go here */}
+                        <p className="text-gray-300">This is where the chat messages will be displayed.</p>
+                    </div>
+                </div>
             </div>
+
         </div>
     );
 }
