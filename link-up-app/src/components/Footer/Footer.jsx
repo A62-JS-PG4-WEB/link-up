cec5ff1dc0da701f5458a528330c465eeef2b16f
Ivo <hristovivo24@gmail.com>
2024-09-10 17:48:25 +0300

    adding correct info&pics for the creators, adding imaginable review from imaginable company

--- link-up-app/src/components/Footer/Footer.jsx
+++ link-up-app/src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export function Footer() {
                 <nav className="grid grid-flow-col gap-4">
                     <a className="link link-hover">About us</a>
                     <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Careers</a>
                     <a className="link link-hover">Press kit</a>
                 </nav>
                 <nav>
--- link-up-app/src/components/Review/Review.jsx
+++ link-up-app/src/components/Review/Review.jsx
@@ -5,24 +5,25 @@ export function Review() {
             <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
             <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
             <div className="mx-auto max-w-2xl lg:max-w-4xl">
-                <img alt="" src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg" className="mx-auto h-12" />
+                <img alt="" src="https://imagizer.imageshack.com/v2/1600x1200q70/923/txctIr.png" className="mx-auto h-24" />
                 <figure className="mt-10">
                     <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
                         <p>
-                            “This application has been an invaluable tool for collaborating with my team. It’s intuitive and efficient, making communication seamless and enjoyable. Despite occasional minor glitches, the overall experience is highly positive and greatly enhances our productivity.”                        </p>
+                            “This application has been an invaluable tool for collaborating with my team. It’s intuitive and efficient, making communication seamless and enjoyable. The overall experience is highly positive and greatly enhances our productivity.”
+                        </p>
                     </blockquote>
                     <figcaption className="mt-10">
                         <img
                             alt=""
-                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            className="mx-auto h-10 w-10 rounded-full"
+                            src="https://imagizer.imageshack.com/v2/1600x1200q70/537/rXSlgb.jpg"
+                            className="mx-auto h-12 w-12 rounded-full"
                         />
                         <div className="mt-4 flex items-center justify-center space-x-3 text-base">
                             <div className="font-semibold text-gray-900">Gergana Mihaleva</div>
                             <svg width={3} height={3} viewBox="0 0 2 2" aria-hidden="true" className="fill-gray-900">
                                 <circle r={1} cx={1} cy={1} />
                             </svg>
-                            <div className="text-gray-600">CEO of Workcation</div>
+                            <div className="text-gray-600">CEO of MagicFilms</div>
                         </div>
                     </figcaption>
                 </figure>
