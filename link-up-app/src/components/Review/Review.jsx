65ef75f8ae53196179f1bf1318c7418509d5911e
Ivo <hristovivo24@gmail.com>
2024-09-10 19:09:10 +0300

    photos added, style too

--- link-up-app/src/components/Review/Review.jsx
+++ link-up-app/src/components/Review/Review.jsx
@@ -2,7 +2,7 @@ export function Review() {
 
     return (
         <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
-            <div className="absolute inset-0 -z-10 bg-[radial-gradient(105rem_40rem_at_bottom,theme(colors.indigo.800),white)] opacity-80" />
+            <div className="absolute inset-0 -z-10 bg-[radial-gradient(105rem_40rem_at_bottom,theme(colors.indigo.100),white)] opacity-80" />
             <div className="absolute inset-y-0 left-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
             <div className="mx-auto max-w-2xl lg:max-w-4xl text-center">
                 <img alt="" src="https://imagizer.imageshack.com/v2/1600x1200q70/923/txctIr.png" className="mx-auto h-24" />
