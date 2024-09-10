83ab19821c07f58cb88154321d2d0e25da1b0c41
Ivo <hristovivo24@gmail.com>
2024-09-10 12:55:12 +0300

    loading effect changed

--- link-up-app/src/hoc/Authenticated.jsx
+++ link-up-app/src/hoc/Authenticated.jsx
@@ -8,9 +8,11 @@ export default function Authenticated({ children }) {
   const location = useLocation();
 
   if (user === null) {
-    return <div>Loading...</div>;
+    return <div className="loading-container">
+      <span className="loading loading-dots loading-lg"></span>
+    </div>
+      ;
   }
-  
   if (!user) {
     return <Navigate replace to="/login" state={{ from: location }} />
   }
