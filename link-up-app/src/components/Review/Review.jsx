fe98dff90a2f8b1a4397a99c2f85b6778f1e97a4
Ivo <hristovivo24@gmail.com>
2024-09-11 01:42:33 +0300

    site hosted v1

--- link-up-app/src/components/Review/Review.jsx
+++ link-up-app/src/components/Review/Review.jsx
@@ -37,6 +37,7 @@ export function Review() {
 
     return (
         <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
+            <p className="text-center text-3xl font-bold text-gray-900 mb-10">Reviews</p> {/* Reviews paragraph added */}
             <div className="absolute inset-0 -z-10 bg-[radial-gradient(105rem_40rem_at_bottom,theme(colors.indigo.100),white)] opacity-80" />
             <div className="absolute inset-y-0 left-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
             <div className="mx-auto max-w-2xl lg:max-w-4xl text-center">
