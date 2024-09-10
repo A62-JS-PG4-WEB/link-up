ec4e9a28015fdb44033b6593671026ea08025e8d
Ivo <hristovivo24@gmail.com>
2024-09-10 12:57:53 +0300

    follow up commit, untracked file added now

--- link-up-app/src/hoc/Authenticated.jsx
+++ link-up-app/src/hoc/Authenticated.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { useContext } from 'react';
 import { AppContext } from '../state/app.context';
 import { Navigate, useLocation } from 'react-router-dom';
-
+import '../index.css'
 export default function Authenticated({ children }) {
   const { user } = useContext(AppContext);
   const location = useLocation();
